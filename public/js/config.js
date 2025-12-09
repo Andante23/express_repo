783fec9c7f803537d10d6e64bc0b6dd4117edee3
andante23 <andante1104@naver.com>
2025-12-09 15:29:06 +0900

    env 파일을 config.js를 통해 불러옴

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const API_PATH = process.env.API_PATH ;
+export const PORT = process.env.PORT ;
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,42 +1,37 @@
-const express = require('express');
-require('dotenv').config();
+
+import express from 'express';
+import fs  from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { API_PATH , PORT } from './public/js/config.js';
+
 const app = express();
-const fs = require('fs');
-const path = require('path');
-
-
-// 정적 파일 제공을 위한 미들웨어 설정
 app.use(express.static('public'));
-
-// ejs 템플릿 설정
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'view'));
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
 
 
 
-// 유저가 / 경로로 접속했을 때 index.html 파일을 제공합니다.
-app.get('/', (req, res,next) => {
- res.sendFile(path.join(__dirname, 'webpage', 'index.html') , function (err) {
-  // 에러가 발생하면 500 상태 코드와 에러 메시지를 로그파일에 남김니다.
-    if (err) {  next(err);}
-  }); 
-});
+// JSON API
+app.get(`${API_PATH}`, (req, res) => {
+ 
+  const filePath = path.join(__dirname, 'data', 'sample.json');
+ 
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error("발생한 에러:", err);  // 추가!!
+      return res.status(500).json({ error: '파일 읽기 오류' });
+    }
 
-app.use((err, req, res, next) => {
-  res.status(500).render('error', {
-    message: err.message,
-    stack: err.stack
+    res.json(JSON.parse(data));
   });
 });
 
 
-
-
-
-const  PORT  = process.env.PORT ;
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
