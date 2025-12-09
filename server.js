2e68da983eba072048ce8f3fb8aa73aec34bc27e
andante23 <andante1104@naver.com>
2025-12-09 15:41:15 +0900

    코드에 주석 추가

--- server.js
+++ server.js
@@ -4,34 +4,47 @@ import fs  from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// 환경변수 를 config,js에서 불러오기
+import { API_PATH , PORT } from './public/js/config.js';
+
+
+// 모듈JS에서 __filename ,__dirname을 사용하기 위해 fileURLToPath와 path 모듈을 사용
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import { API_PATH , PORT } from './public/js/config.js';
-
+// Express 애플리케이션 생성
 const app = express();
+
+
+// 이미지, css , js 등 정적 파일 제공
 app.use(express.static('public'));
+
+// 미들웨어
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true }));
 
 
 
-// JSON API
+// 샘플 JSON 파일을 읽어와서 응답하는 API 엔드포인트
 app.get(`${API_PATH}`, (req, res) => {
  
   const filePath = path.join(__dirname, 'data', 'sample.json');
  
+  // json 파일 읽기
   fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
+    // json 파일 읽기 오류 처리
+    if (err) { 
       console.error("발생한 에러:", err);  // 추가!!
-      return res.status(500).json({ error: '파일 읽기 오류' });
+      return res.status(500).json({ error: err.message });
     }
 
+    // json 파일 파싱 및 응답
     res.json(JSON.parse(data));
   });
 });
 
 
+// 서버 시작
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
